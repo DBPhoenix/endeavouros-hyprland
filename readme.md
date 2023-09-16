3c223d3c3527abcf654a3c1f3cb3faba32a542cf
Phoenix <danielbirn@hotmail.com>
2023-09-17 01:02:43 +0200

    adding dotfiles

---
+++ readme.md
@@ -0,0 +1,9 @@
+# TODO
+- Post install set xkeymap
+- Waybar Hyprland
+- Screen Sharing
+
+# Non Pacman packages
+- waybar-hyprland
+- wev
+- hyprpicker
--- user_commands.bash
+++ user_commands.bash
@@ -4,11 +4,18 @@
 pre_install() {
     git clone https://github.com/DBPhoenix/endeavouros-hyprland.git
     patch -d /etc/calamares -p0 < endeavouros-hyprland/patch.diff
-    rm -rf endeavouros-hyprland
 }
 
 post_install() {
-    :
+    chown $1:$1 -R endeavouros-hyprland/.config
+    cp -R endeavouros-hyprland/.config/* /home/$1/.config/
+    rm -rf endeavouros-hyprland
+
+    yay -S waybar-hyprland wev hyprpicker --noconfirm --answerdiff=None
+
+    echo '\nsource = ~/.config/hypr/eos.conf' >> /home/$1/.config/hypr/hyprland.conf
+
+    systemctl enable mpd.service
 }
 
 if [ $# -eq 1 ]
